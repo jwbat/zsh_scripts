e66c9990cf81e48464a5fd174caff91cb68d6bd0
jwbat <jwesleyb@gmail.com>
2023-06-03 17:28:06 -0700

    .

---
+++ info/cr.sql
@@ -0,0 +1,28 @@
+SELECT count(*) FROM ubx_asset;
+SELECT count(*) FROM ubx_device;
+SELECT count(*) FROM ubx_device_registry;     -- 73
+    SELECT * FROM ubx_device_registry;
+SELECT count(*) FROM ubx_building;
+SELECT count(*) FROM gql_state_variables;
+SELECT count(*) FROM ipc_registry;
+SELECT count(*) FROM ubx_account;
+SELECT count(*) FROM ubx_account_manufacturer;
+SELECT count(*) FROM ubx_asset_category;
+SELECT count(*) FROM ubx_asset_type;
+SELECT count(*) FROM ubx_building_group;
+SELECT count(*) FROM ubx_building_group_map;
+SELECT count(*) FROM ubx_device_cluster_map;
+SELECT count(*) FROM ubx_document;
+SELECT count(*) FROM ubx_floor;
+SELECT count(*) FROM ubx_floorplan;
+SELECT count(*) FROM ubx_known_asset;           -- 19
+    SELECT * FROM ubx_known_asset;
+SELECT count(*) FROM ubx_known_manufacturer;
+SELECT count(*) FROM ubx_permission_account;
+SELECT count(*) FROM ubx_permission_building;
+SELECT count(*) FROM ubx_permission_building_group;
+SELECT count(*) FROM ubx_threshold;
+SELECT count(*) FROM ubx_user;
+SELECT count(*) FROM ubx_user_invitation;
+SELECT count(*) FROM ubx_usergroup;
+SELECT count(*) FROM ubx_usergroup_access;
